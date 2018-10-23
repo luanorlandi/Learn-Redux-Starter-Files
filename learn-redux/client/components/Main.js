97b2ae99ba7c59f2b95bdb281c4bf1099b91bce1
Luan Orlandi <orlandi.luan@gmail.com>
2018-10-23 17:02:58 -0300

    02 apply layout

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const Main = React.createClass({
+class Main extends React.Component {
   render() {
     return (
       <div>
         <h1>
           <Link to="/">Reduxstagram</Link>
         </h1>
-        {React.cloneElement(this.props.children, this.props)}
       </div>
-    )
+    );
   }
-});
+}
 
 export default Main;
