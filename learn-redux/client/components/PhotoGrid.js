db8807a7e3650238a8340410a09475012b98fd96
Luan Orlandi <orlandi.luan@gmail.com>
2018-10-23 17:12:57 -0300

    03 create Single and PhotoGrid components

---
+++ learn-redux/client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PhotoGrid extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="photo-grid">grid</div>
+      </div>
+    );
+  }
+}
+
+export default PhotoGrid;
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Single extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="single-photo">single</div>
+      </div>
+    );
+  }
+}
+
+export default Single;
