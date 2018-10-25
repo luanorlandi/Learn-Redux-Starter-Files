58e0f67d1a20b2d6c416031d62c78b6d192bfe26
Luan Orlandi <orlandi.luan@gmail.com>
2018-10-24 22:27:22 -0300

    10 acessing dispatch and state

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -4,7 +4,7 @@ import { Router, Route, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './store';
 
-import Main from './components/Main';
+import App from './components/App';
 import PhotoGrid from './components/PhotoGrid';
 import Single from './components/Single';
 import './styles/style.styl';
@@ -12,7 +12,7 @@ import './styles/style.styl';
 const router = (
   <Provider store={store}>
     <Router history={history}>
-      <Route path="/" component={Main}>
+      <Route path="/" component={App}>
         <IndexRoute component={PhotoGrid} />
         <Route path="/view/:postId" component={Single} />
       </Route>
