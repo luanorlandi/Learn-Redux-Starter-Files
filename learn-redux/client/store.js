3a108f4ded12cebef0ad187496fc35bec528b163
Luan Orlandi <orlandi.luan@gmail.com>
2018-10-25 00:58:35 -0300

    19 redux dev tools

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -11,7 +11,11 @@ const defaultState = {
   comments
 };
 
-const store = createStore(rootReducer, defaultState);
+const store = createStore(
+  rootReducer,
+  defaultState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 export const history = syncHistoryWithStore(browserHistory, store);
 
 if (module.hot) {
