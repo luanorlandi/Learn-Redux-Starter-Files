3530abea5a242500fd420b27c4dc89fe5d8edf5b
Luan Orlandi <orlandi.luan@gmail.com>
2018-10-24 23:56:26 -0300

    15 updating comment state

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -3,7 +3,7 @@ import Photo from './Photo';
 import Comments from './Comments';
 
 const Single = (props) => {
-  const { posts, params, comments } = props;
+  const { posts, params, comments, addComment } = props;
   const { postId } = params;
   const i = posts.findIndex((post) => post.code === postId);
   const post = posts[i];
@@ -13,7 +13,11 @@ const Single = (props) => {
     <div>
       <div className="single-photo">
         <Photo i={i} post={post} {...props} />
-        <Comments postComments={postComments} />
+        <Comments
+          postComments={postComments}
+          postId={postId}
+          addComment={addComment}
+        />
       </div>
     </div>
   );
